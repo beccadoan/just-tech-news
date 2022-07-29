c6a6cc8061073acafbb724ca2e8afa25d2990f13
Becca Doan <rebecca.doan@yahoo.com>
2022-07-29 15:27:46 -0500

    initial commit

---
+++ models/User.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection')
+
+// create User model
+class User extends Model {}
+
+// define table columns and configuration
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: falseß
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        }
+    },
+    {
+        sequelize,
+        // Table configuration options go here
+        // don't automaticall create timestamp fields
+        timestamps: false,
+        // don't pluralize name of database table
+        freezeTableName: true,
+        // use underscores instead of camel-casing
+        underscored: true,
+        // make it so our model name stays lowercase
+        modelName: 'user'
+    }
+)
+
+module.exports = User;
---
+++ routes/api/user-routes.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.get('/', (req, res) => {
+    User.findAll()
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err)
+        })
+});
+
+router.get('/:id', (req, res) => {
+    User.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData){
+            res.status(404).json({ message: 'No user found with this id' })
+            return;
+        }
+    })
+});
+
+router.post('/', (req, res) => {
+
+})
+
+router.put('/:id', (req, res) => {
+
+})
+
+router.delete('/:id', (req, res) => {
+    
+})
+
+module.exports = router;
