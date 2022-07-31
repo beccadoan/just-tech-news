6a7264782f8308f642543cb7a818bd0997ab8571
Becca Doan <rebecca.doan@yahoo.com>
2022-07-31 18:47:32 -0500

    added post request for new post

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
+const postRotues = require('./post-routes')
 
 router.use('/users', userRoutes);
+router.use('/posts', postRotues)
 
 module.exports = router;
