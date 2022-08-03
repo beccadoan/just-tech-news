3fcc40c2d0dcf13268fc7975567b1822f3bd495f
Becca Doan <rebecca.doan@yahoo.com>
2022-08-03 14:09:02 -0500

    added put request for voting on a post

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
-const postRotues = require('./post-routes')
+const postRotues = require('./post-routes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRotues)
