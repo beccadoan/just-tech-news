a2c3d9dbeac6db3ebd4884b23520147b67f28cb8
Becca Doan <rebecca.doan@yahoo.com>
2022-08-03 16:06:45 -0500

    completed upvote method

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -5,7 +5,7 @@ const sequelize = require('../../config/connection')
 router.get('/', (req, res) => {
     console.log('=====================');
     Post.findAll({
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
         order: [['created_at', 'DESC']],
         include: [
             {
@@ -26,7 +26,7 @@ router.get('/:id', (req, res) => {
         where: {
             id: req.params.id
         },
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
         include: [
             {
                 model: User,
@@ -62,24 +62,7 @@ router.post('/', (req, res) => {
 })
 
 router.put('/upvote', (req, res) => {
-    Vote.create({
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-    })
-    .then(() => {
-        return Post.findOne({
-            where: {
-                id: req.body.post_id
-            },
-            attributes: [
-                'id','post_url', 'title', 'created_at',
-                [
-                sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
-                'vote_count'
-                ]
-            ]
-        })
-    })
+    Post.upvote(req.body, { Vote })
     .then(dbPostData => res.json(dbPostData))
     .catch(err => res.json(err))
 })
