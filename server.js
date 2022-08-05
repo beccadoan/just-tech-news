9ba08be514e646ef0475ed307d2460cbe6d04876
Becca Doan <rebecca.doan@yahoo.com>
2022-08-04 23:53:08 -0500

    apopulated templates with database data

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
-
+const exphbs = require('express-handlebars')
+const hbs = exphbs.create({})
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -13,9 +14,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(routes)
-
-const exphbs = require('express-handlebars')
-const hbs = exphbs.create({})
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'))
